86da603dbc4293ff78c841d7bd16010e793187fd
kevser yilmaz <kvsyilmaz98@gmail.com>
2024-05-11 15:08:49 +0300

    API testleri eklendi

---
+++ API/API_constants.py
@@ -0,0 +1,12 @@
+#---------------------URL-----------------------
+BASE_URL= "https://api.tobeto.com/api"
+LOGIN_ENDPOINT= "/auth/local"
+CHANGE_PASSWORD_ENDPOINT= "/auth/change-password"
+REMOVE_ACCOUNT_ENDPOINT= "/user-profile/remove-account"
+
+#--------------------TEXTS---------------------
+INVALID_PASSWORD_ERROR_MESSAGE= 'The provided current password is invalid'
+SHORT_PASSWORD_ERROR_MESSAGE= 'password must be at least 6 characters'
+UNMATCHED_PASSWORD_ERROR_MESSAGE= 'Passwords do not match'
+CURRENT_PASSWORD_ERROR_MESSAGE= 'Your new password must be different than your current password'
+EMPTY_PASSWORD_ERROR_MESSAGE= '3 errors occurred'
